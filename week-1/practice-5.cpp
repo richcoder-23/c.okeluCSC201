ad0d631aeea2907033597b62da5217182e916f3d
richcoder-23 <chinemelum.richard@gmail.com>
2024-12-14 13:49:20 +0100

    (My first commit)

---
+++ week-1/practice-5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void printValue(string val)
+{
+    cout << val;
+}
+
+int main(){
+
+        printValue("Hello, World!");
+        return 0;
+}
---
+++ week-1/practice-9.cpp
@@ -0,0 +1,15 @@
+/* defined constants: calculate circumference*/
+
+#include <iostream>
+    using namespace std;
+#define PI 3.14159
+
+int main()
+
+{
+    double r = 5.0; //radius
+    double circle;
+    circle = 2 * PI * r;
+    cout << circle;
+    return 0;
+}
